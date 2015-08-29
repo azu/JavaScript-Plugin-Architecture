86a15df1c47bb27b53ea5dc3110a5c008d5e1bab
azu <azuciao@gmail.com>
2015-08-29 23:16:45 +0900

    feat(jQuery): jQueryプラグインのサンプルとテストを追加

--- src/jQuery/jquery.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-import jQuery from "jquery";
-jQuery(document.body);
---
+++ test/jQuery/greenify-test.js
@@ -0,0 +1,28 @@
+// LICENSE : MIT
+"use strict";
+import jsdom from "jsdom";
+import assert from "power-assert"
+import fs from "fs";
+const testbed = fs.readFileSync(__dirname + "/fixtures/testbed.html", "utf-8");
+const jquery = fs.readFileSync(__dirname + "/../../node_modules/jquery/dist/jquery.js", "utf-8");
+const greenify = fs.readFileSync(__dirname + "/../../src/jQuery/greenify.js", "utf-8");
+describe("greenify", function () {
+    var $, document;
+    before(done => {
+        jsdom.env({
+            html: testbed,
+            src: [jquery, greenify],
+            done: function (err, window) {
+                document = window.document;
+                $ = window.$;
+                done();
+            }
+        });
+    });
+    it("should extend $.prototype with greenify", function () {
+        assert(typeof $ !== "undefined");
+        assert($.fn.greenify != null);
+        assert($(document.body).greenify != null);
+        assert($(document.body).greenify() instanceof $);
+    });
+});
