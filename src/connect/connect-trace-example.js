7bbc99c6aba178f93d204b3d5224dccbc7a5256f
azu <azuciao@gmail.com>
2015-09-15 21:27:35 +0900

    fix(connect): Lintの結果を修正
    冗長な文章の修正
    varではなくletを使う

--- src/connect/connect-trace-example.js
+++ src/connect/connect-trace-example.js
@@ -2,10 +2,8 @@
 import errorHandler from "./errorHandler";
 import hello from "./hello";
 import nosniff from "./nosniff";
-import assert from "assert";
 import connect from "connect";
 import http from "http";
-import fetch from "node-fetch";
 
 const responseText = "response text";
 let app = connect();
