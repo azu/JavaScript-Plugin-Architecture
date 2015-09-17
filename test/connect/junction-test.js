783fd57d7052bc50bad5041d7f3f080024029169
azu <azuciao@gmail.com>
2015-09-17 10:40:32 +0900

    test(connect): add junction test

--- test/connect/junction-test.js
+++ test/connect/junction-test.js
@@ -3,8 +3,8 @@
 import assert from "power-assert";
 import Junction from "../../src/connect/junction";
 describe("junction", function () {
-    describe("when", function () {
-        it("should", function (done) {
+    context("when register middlewares", function () {
+        it("should connect middleware, the order is register", function (done) {
             var junction = new Junction();
             junction.use(function errorHandling(error, text, next) {
                 next(error);
@@ -24,4 +24,23 @@ describe("junction", function () {
             });
         });
     });
+    context("when occur error in middleware", function () {
+        it("should call errorHandling middleware", function (done) {
+            var junction = new Junction();
+            junction.use(function toUpper(text, next) {
+                throw new Error("ROL");
+            });
+            // TODO: 順番に依存してる
+            junction.use(function errorHandling(error, text, next) {
+                assert(error instanceof Error);
+                done();
+            });
+            junction.process("text", (error, result) => {
+                if (error) {
+                    return done(error);
+                }
+                done();
+            });
+        });
+    })
 });
