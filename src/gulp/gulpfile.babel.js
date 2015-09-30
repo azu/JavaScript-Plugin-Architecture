e264b62fe30eb0ee807880c196a0b234cf810c8c
azu <azuciao@gmail.com>
2015-09-30 09:59:23 +0900

    refactor(gulp): add Buffer version

--- src/gulp/gulpfile.babel.js
+++ src/gulp/gulpfile.babel.js
@@ -2,31 +2,45 @@
 "use strict";
 import gulp from "gulp";
 import {Transform} from "stream";
-let transformStream = new Transform({
-    writableObjectMode: true,
-    readableObjectMode: true,
-    transform: function (chunk, encoding, next) {
-        let str = Buffer.concat([Buffer("prefix"), chunk]);
-        this.push(str);
-        next();
-    }
-});
 
-let gulpTransform = new Transform({
-    writableObjectMode: true,
-    readableObjectMode: true,
-    transform: function (file, encoding, next) {
-        if (file.isStream()) {
-            file.contents = file.contents.pipe(transformStream);
+let prefixBuffer = function (buffer, prefix) {
+    return Buffer.concat([Buffer(prefix), buffer]);
+};
+
+let prefixStream = function (prefix) {
+    return new Transform({
+        transform: function (chunk, encoding, next) {
+            let buffer = prefixBuffer(chunk, prefix);
+            this.push(buffer);
+            next();
         }
-        this.push(file);
-        next();
-    }
-});
+    });
+};
+
+let gulpTransform = function (prefix) {
+    // enable `objectMode` of the stream for vinyl File objects.
+    return new Transform({
+        // Takes in vinyl File objects
+        writableObjectMode: true,
+        // Outputs vinyl File objects
+        readableObjectMode: true,
+        transform: function (file, encoding, next) {
+            if (file.isBuffer()) {
+                file.contents = prefixBuffer(file.contents, prefix);
+            }
+
+            if (file.isStream()) {
+                file.contents = file.contents.pipe(prefixStream(prefix));
+            }
+            this.push(file);
+            next();
+        }
+    });
+};
 gulp.task("default", function () {
-    return gulp.src("./*.js", {buffer: false})
-        .pipe(gulpTransform)
-        .pipe(gulp.dest("modified-files"))
+    return gulp.src("./*.*")
+        .pipe(gulpTransform("prefix text"))
+        .pipe(gulp.dest("build"))
         .on("error", (error) => {
             console.error(error);
         });
