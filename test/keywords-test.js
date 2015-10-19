ee6edfc53af95f34f430b4fb50dc157b42599bc3
azu <azuciao@gmail.com>
2015-10-19 23:05:34 +0900

    chore(npm): cleanup

--- test/keywords-test.js
+++ test/keywords-test.js
@@ -1,7 +1,6 @@
 // LICENSE : MIT
 "use strict";
 import {getKeywords}  from "stemming-x-keywords";
-import {getFilePathListAsync}  from "gitbook-summary-to-path";
 import path from "path";
 import fs from "fs";
 import mdast from "mdast";
@@ -9,8 +8,9 @@ import parents from "unist-util-parents";
 import select from "unist-util-select";
 import isUnist from "unist-util-is";
 import nlcstToString  from "nlcst-to-string";
+
 const rootDir = path.join(__dirname, "..");
-const OrganizationText = fs.readFileSync(__dirname + "/../ORGANIZATION.md", "utf-8");
+const OrganizationText = fs.readFileSync(path.join(rootDir, "ORGANIZATION.md"), "utf-8");
 function isNotContain(content, keywords) {
     // 含んでないものだけを返す
     return keywords.filter(keyword => {
@@ -42,46 +42,48 @@ function isAlreadyCheckKeyword(list, keyword) {
     return list.indexOf(keyword) !== -1;
 }
 
+function checkKeyword(text){
+    let ast = mdast.parse(text);
+    let headerLinks = select(parents(ast), "heading link[href]");
+    let paragraphList = headerLinks.map(link => {
+        let filePath = path.resolve(rootDir, link.href);
+        let paragraphs = findAllAfter(ast, link.parent.node, "paragraph");
+        let results = {
+            filePath: filePath,
+            content: fs.readFileSync(filePath, "utf-8"),
+            keywords: []
+        };
+        let keywords = paragraphs.map(p => {
+            let text = nlcstToString(p);
+            return getKeywords(text).then(a => {
+                results.keywords = results.keywords.concat(a);
+            });
+        });
+        return Promise.all(keywords).then(()=> {
+            return results;
+        });
+    });
+    return Promise.all(paragraphList).then(results => {
+        let confirmedKeywords = [];
+        return results.forEach(({filePath, content, keywords}) => {
+            let unusedKeywords = isNotContain(content, keywords);
+            let isChecked = isAlreadyCheckKeyword.bind(null, confirmedKeywords);
+            if (unusedKeywords.length === 0) {
+                // 使用済みのキーワードを登録
+                confirmedKeywords = confirmedKeywords.concat(keywords);
+                return;
+            }
+            if (unusedKeywords.every(isChecked)) {
+                console.log(unusedKeywords.join(",") + "はチェック済み");
+                return;
+            }
+            throw new Error(`"${unusedKeywords.join(",")}" are not used in ${filePath}`);
+        });
+    });
+}
 // キーワードが書くコンテンツに含まれているかをテストする
 describe("keywords", function () {
     it("Each chapter contain the keyword", function () {
-        let ast = mdast.parse(OrganizationText);
-        let headerLinks = select(parents(ast), "heading link[href]");
-        let paragraphList = headerLinks.map(link => {
-            let filePath = path.resolve(rootDir, link.href);
-            let paragraphs = findAllAfter(ast, link.parent.node, "paragraph");
-            let results = {
-                filePath: filePath,
-                content: fs.readFileSync(filePath, "utf-8"),
-                keywords: []
-            };
-            let keywords = paragraphs.map(p => {
-                let text = nlcstToString(p);
-                return getKeywords(text).then(a => {
-                    results.keywords = results.keywords.concat(a);
-                });
-            });
-            return Promise.all(keywords).then(()=> {
-                return results;
-            });
-        });
-        let promises = Promise.all(paragraphList).then(results => {
-            let confirmedKeywords = [];
-            return results.forEach(({filePath, content, keywords}) => {
-                let unusedKeywords = isNotContain(content, keywords);
-                let isChecked = isAlreadyCheckKeyword.bind(null, confirmedKeywords);
-                if (unusedKeywords.length === 0) {
-                    // 使用済みのキーワードを登録
-                    confirmedKeywords = confirmedKeywords.concat(keywords);
-                    return;
-                }
-                if (unusedKeywords.every(isChecked)) {
-                    console.log(unusedKeywords.join(",") + "はチェック済み");
-                    return;
-                }
-                throw new Error(`"${unusedKeywords.join(",")}" are not used in ${filePath}`);
-            });
-        });
-        return promises;
+        return checkKeyword(OrganizationText);
     });
 });
