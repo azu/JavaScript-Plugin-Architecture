2a30b7db8fca6e72ffc5214247c173d7860ecc00
azu <azuciao@gmail.com>
2016-05-01 17:33:54 +0900

    feat(redux): add action logger

---
+++ test/redux/action-logger-test.js
@@ -0,0 +1,32 @@
+// LICENSE : MIT
+"use strict";
+const assert = require("power-assert");
+import {createStore, applyMiddleware} from "redux";
+import createLogger from "../../src/redux/action-logger";
+const initialState = {};
+const reducer = (state = initialState, action) => state;
+
+describe("logger-test", function () {
+    let store, dispatch, logs;
+    beforeEach(() => {
+        logs = [];
+        const con = {
+            log(message){
+                logs.push(message);
+            }
+        };
+        const middleware = createLogger({
+            logger: con
+        });
+        store = applyMiddleware(middleware)(createStore)(reducer);
+        dispatch = store.dispatch;
+    });
+    afterEach(() => {
+        logs.length = 0;
+    });
+    it("should output log", function () {
+        const action = {type: "FOO"};
+        dispatch(action);
+        assert.deepEqual(logs, [action]);
+    });
+});
