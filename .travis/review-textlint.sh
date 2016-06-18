f6a2ceac7512c60321ae93fc4062cd273bf031a8
azu <azuciao@gmail.com>
2016-06-18 19:46:30 +0900

    use ruby

--- .travis/review-textlint.sh
+++ .travis/review-textlint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# Test if pull request
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] || [ -z "$TRAVIS_PULL_REQUEST" ]; then
   echo 'not pull request.'
   exit 0
 fi
-# fetch other branch
+
+# Fetch other branch
+# To avoid ambiguous argument
+# http://stackoverflow.com/questions/37303969/git-fatal-ambiguous-argument-head-unknown-revision-or-path-not-in-the-workin
 if [ "$TRAVIS" == "true" ]; then
   #resolving `detached HEAD` by attaching HEAD to the `TRAVIS_FROM_BRANCH` branch
   TRAVIS_FROM_BRANCH="travis_from_branch"
@@ -20,15 +24,18 @@ if [ "$TRAVIS" == "true" ]; then
   #switch to `TRAVIS_FROM_BRANCH`
   git checkout $TRAVIS_FROM_BRANCH
 fi
+
 # Install saddler
 # https://github.com/packsaddle/ruby-saddler
 # Need secret env: `GITHUB_ACCESS_TOKEN=xxx`
 gem install --no-document checkstyle_filter-git saddler saddler-reporter-github
+
 # Diff Target Branch
 # diff HEAD...target
 # http://stackoverflow.com/questions/3161204/find-the-parent-branch-of-a-git-branch
 # http://qiita.com/upinetree/items/0b74b08b64442f0a89b9
-diffBranchName=$(git show-branch | grep '*' | grep -v "$(git rev-parse --abbrev-ref HEAD)" | head -1 | awk -F'[]~^[]' '{print $2}')
+declare diffBranchName=$(git show-branch | grep '*' | grep -v "$(git rev-parse --abbrev-ref HEAD)" | head -1 | awk -F'[]~^[]' '{print $2}')
+
 # filter files and lint
 echo "${diffBranchName}...HEAD"
 echo "textlint -> review_comments"
